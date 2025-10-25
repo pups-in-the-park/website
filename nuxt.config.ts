7f4a4b3bd6f86445aad84bd6a27e5f4259aac219
MattA-Official <43476212+MattA-Official@users.noreply.github.com>
2025-10-25 16:45:53 +0100

    re-order modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-
   modules: [
-    '@nuxt/content',
     '@nuxt/eslint',
     '@nuxt/image',
-    '@nuxt/ui'
-  ]
+    '@nuxt/ui',
+    '@nuxt/content'
+  ],
+
+  devtools: { enabled: true },
+
+  compatibilityDate: '2025-07-15'
 })
